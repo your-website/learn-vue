404df707f71bdd7129ffb4082a4814f6d62b5983
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-02 12:12:47 +0700

    add BaseSelect

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,10 +2,12 @@
   <div>
     <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect
+        label="Select a category"
+        v-model="event.category"
+        :options="categories"
+        class="field"
+      />
 
       <h3>Name & describe your event</h3>
       <BaseInput
@@ -41,12 +43,12 @@
         <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
 
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+      <BaseSelect
+        label="Select a time"
+        v-model="event.time"
+        :options="times"
+        class="field"
+      />
 
       <input type="submit" class="button -fill-gradient" value="Submit" />
     </form>
